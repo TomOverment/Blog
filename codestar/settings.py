530e33977fbb52bc0f3df204dc37f70184523810
TomOverment Overment <tomozart22@outlook.com>
2024-06-24 13:55:58 +0000

    final2

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['8000-tomoverment-blog-qahvl6dgb00.ws-eu114.gitpod.io','8000-tomoverment-blog-jnft9um2t9i.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
